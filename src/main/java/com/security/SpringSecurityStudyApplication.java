ab039bd695bcb8df27a2bd92ef4471104b50bc4d
Yonghwi Kim <yh0921k@gmail.com>
2020-12-24 08:59:34 +0900

    [init] Spring Security 학습을 위한 프로젝트 초기화 및 설정
    - Gradle, Java 11, IntelliJ 사용
    - 초기 의존성 Spring Web, Thymeleaf 추가
    
    .gitignore 제외 설정 추가
    - macos,windows,linux,intellij,java,database,git,gradle,maven,visualstudiocode

---
+++ src/main/java/com/security/SpringSecurityStudyApplication.java
@@ -0,0 +1,12 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityStudyApplication.class, args);
+  }
+}
